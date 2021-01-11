bfd24706e2b7cece00b83c5ff3b83e42e5e097f0
Thiago Ferreira <62024096+thiagosouf@users.noreply.github.com>
2021-01-11 17:38:04 -0300

    Add files via upload
    2commit

--- zapbot.py
+++ zapbot.py
@@ -3,7 +3,7 @@ import time
 
 class WhatsappBot:
     def __init__(self):
-        self.mensagem = "Teste"
+        self.mensagem = ["Teste"]
         self.grupos = ["Trabalho", "Cinema - Técnica"]
         options = webdriver.ChromeOptions()
         options.add_argument("lang=pt-br")
@@ -15,21 +15,30 @@ class WhatsappBot:
             #<div tabindex="-1" class="DuUXI">
                 #<span data-testid="send" data-icon="send" class="">
         self.driver.get("https://web.whatsapp.com/")   
-        time.sleep(5)     
+        time.sleep(5)
+        a=0     
         for grupo in self.grupos:
             print("enviando msg para:", grupo)
-            #localizador = self.driver.find_element_by_class_name('_2HE1Z _1aIlm')
-            grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
             time.sleep(5)
+            localizador = self.driver.find_element_by_xpath("//div[@class='_1awRl copyable-text selectable-text']")
+            time.sleep(8)
+            localizador.click()
+            time.sleep(5)
+            localizador.send_keys(self.grupos[a])
+            grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
+            time.sleep(8)
             grupo.click()
             chat_box = self.driver.find_element_by_class_name('DuUXI')
-            time.sleep(5)
+            time.sleep(8)
             chat_box.click()
-            chat_box.send_keys(self.mensagem)
-            botao_enviar = self.driver.find_element_by_xpath('//span[@data-icon="send"]')
+            time.sleep(5)
+            chat_box.send_keys(self.mensagem[0])
+            botao_enviar = self.driver.find_element_by_xpath("//span[@data-icon='send']")
             time.sleep(5)
             botao_enviar.click()
-            time.sleep(5)
+            time.sleep(8)
+            a=a+1
+            print(a)
         
 bot = WhatsappBot()
 bot.EnviarMensagens()

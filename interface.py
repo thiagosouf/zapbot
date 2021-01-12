743c323da2d38692a100288087cb2b36117e374d
Thiago Ferreira <62024096+thiagosouf@users.noreply.github.com>
2021-01-12 16:57:51 -0300

    Add files via upload
    tchau

--- interface.py
+++ interface.py
@@ -1,25 +1,14 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+def abrirArquivo():
+    arquivo = filedialog.askopenfilename(filetypes = ())
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+window = Tk()
+window.title("ZapBot")
+window.geometry('350x500')
 
-        self.quit = tk.Button(self, text="FECHAR", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+botaoAbrir = Button(window, text="Abrir Arquivo", command = abrirArquivo)
+botaoAbrir.pack()
 
-    def say_hi(self):
-        print("hi there, everyone!")
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+window.mainloop()
